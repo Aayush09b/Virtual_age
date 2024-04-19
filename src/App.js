206ab239c9d396457cbb9d6cbe8bfbceab3f32dd
Your Name <you@example.com>
2024-04-19 07:59:02 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import HealthForm from "./screens/HealthForm";
+import Result from "./screens/Result";
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/HealthForm" element={<HealthForm />} />
+          <Route exact path="/health-score" element={<Result />} />
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
